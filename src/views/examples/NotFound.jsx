de28b5cb50b70bf00396f96e91a9d76f2590c452
ValdoNeves <valdogt1@gmail.com>
2021-08-02 18:49:22 -0300

    definindo leitura de routers e paginas nao encontradas

---
+++ src/views/examples/NotFound.jsx
@@ -0,0 +1,11 @@
+import './NotFound.css'
+import React from 'react'
+
+const NotFound = props => (
+    <div>
+        <h1>404</h1>
+        <h2>Opsss...Pagina não encontrada</h2>
+    </div>
+)
+
+export default NotFound
